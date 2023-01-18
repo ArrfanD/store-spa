da10243af4dfbc5b7bd7d0a10c1a8e684d8a7b86
Arrfan Dannge <90389747+ArrfanD@users.noreply.github.com>
2023-01-19 02:24:45 +0530

    Product data shown in the Product Details page

--- store-spa/src/components/ProductPage/ProductPage.js
+++ store-spa/src/components/ProductPage/ProductPage.js
@@ -5,9 +5,22 @@ import { useSelector } from 'react-redux'
 const ProductPage = () => {
     let {cart : { product, selectedProduct }} = useSelector((state)=> state)
     const sortedProduct = product.filter(x=> x.id === selectedProduct)
-    console.log('SORTED SORTED SORTED', sortedProduct)
+    let { id,
+        title,
+        price,
+        description,
+        category,
+        image,
+        amount, } = sortedProduct[0]
   return (
-    <div>ProductPage</div>
+    <div>
+        <h1>{title}</h1>
+        <h1>{price}</h1>
+        <h1>{description}</h1>
+        <h1>{category}</h1>
+        <img src={image} alt="image" />
+        <h1>{amount}</h1>
+    </div>
   )
 }
 
